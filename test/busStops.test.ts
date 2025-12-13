181b81f158746019e1d4e79233d6e9eab8391116
Gnlow <gnlow@pm.me>
2025-12-13 23:50:39 +0900

    Update busStops.test.ts

--- test/busStops.test.ts
+++ test/busStops.test.ts
@@ -39,6 +39,5 @@ const res = bytes(busStopsRaw)
         도시명:        z.string(),
         관리도시명:     z.string(),
     }))
-    .pass(x => console.log(x.toArray()))
 
 console.log(res.raw.toArray().length)
